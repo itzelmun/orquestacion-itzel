17db8aed01c4abf574936405bf6f79bbb64b4374
itzelmun <isanchez17@ucol.mx>
2023-01-24 20:35:58 +0000

    cambio en carpeta phpmyadmin

--- proyecto/config.php
+++ proyecto/config.php
@@ -6,7 +6,7 @@
 	// url de nuestro servidor, en este caso, carpeta demo.
     $SP_URL = 'https://'.$_SERVER['SERVER_NAME']."/demo/";  
     // Fuente de autenticacion definida en el authsources del SP ej, default-sp
-	$SP_ORIGEN= 'datacenter';   
+	$SP_ORIGEN= 'desarrollo4sistemas';   
     // Se crea la instancia del saml, pasando como parametro la fuente de autenticacion.
 	$saml = new SimpleSAML_Auth_Simple($SP_ORIGEN);   
 ?>
