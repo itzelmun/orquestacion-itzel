3f15a5b8f5300f0e68de2191a26d849ee520caa4
itzelmun <isanchez17@ucol.mx>
2023-01-24 21:19:02 +0000

    cambios

--- proyecto/simplesaml/config/authsources.php
+++ proyecto/simplesaml/config/authsources.php
@@ -24,6 +24,13 @@ $config = [
 
     // An authentication source which can authenticate against both SAML 2.0
     // and Shibboleth 1.3 IdPs.
+   
+
+
+
+
+
+
     'proyecto' => [
         'saml:SP',
 
@@ -38,8 +45,8 @@ $config = [
         // The URL to the discovery service.
         // Can be NULL/unset, in which case a builtin discovery service will be used.
         'discoURL' => null,
-	    'certificate' => 'localhost.crt',
-            'privatekey' => 'localhost.key',
+	    'certificate' => 'UCOL.crt',
+            'privatekey' => 'UCOL.key',
 
             // All communications are encrypted and signed
             'redirect.sign' => TRUE,
@@ -47,6 +54,14 @@ $config = [
             //'assertion.encryption' => TRUE,
 	     'name'=>'Proyecto demo',
     ],
+
+
+
+
+
+
+
+
 
     'default-sp' => [
         'saml:SP',
